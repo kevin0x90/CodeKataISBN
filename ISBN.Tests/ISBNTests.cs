63f0ab790ce8bb83409de46e3d58704e924d1410
kevin0x90 <k3v1n0x90@gmail.com>
2017-07-12 13:25:40 +0200

    Initial checkin

---
+++ ISBN.Tests/ISBNTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISBN.Tests
+{
+    [TestFixture]
+    public class ISBNTests
+    {
+        [Test]
+        [TestCase("", Description = "An empty ISBN")]
+        [TestCase("9A8-0-1Y-1495O5-1", Description = "ISBN with invalid characters")]
+        [TestCase("978-0-13-149505-1", Description = "ISBN with invalid check digit")]
+        [TestCase("978 0-262 13472-9", Description = "ISBN with mixed space and dash seperators")]
+        public void ISBN_GivenAnInvalidISBN_Then_ItShouldReturnFalse(string invalidISBN)
+        {
+            // Arrange
+            var sut = new ISBNChecker();
+
+            // Act
+            var isValidISBN = sut.IsValidISBN(invalidISBN);
+
+            // Assert
+            Assert.That<bool>(isValidISBN, Is.False);
+        }
+
+        [Test]
+        [TestCase("9780470059029", Description = "ISBN without seperators")]
+        [TestCase("978 0 471 48648 0", Description = "ISBN with space as seperator")]
+        [TestCase("978-0596809485", Description = "ISBN with dash as seperator")]
+        [TestCase("978-0-13-149505-0", Description = "ISBN with multiple dash seperators")]
+        [TestCase("978-0-262-13472-9", Description = "ISBN with multiple dash seperators")]
+        public void ISBN_GivenValidISBN_Then_ItShouldReturnTrue(string isbn)
+        {
+            // Arrange
+            var sut = new ISBNChecker();
+
+            // Act
+            var isValidISBN = sut.IsValidISBN(isbn);
+
+            // Assert
+            Assert.That<bool>(isValidISBN, Is.True);
+        }
+    }
+}
---
+++ ISBN/ISBNChecker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISBN
+{
+    public class ISBNChecker
+    {
+        private const int ISBN_13_LENGTH = 13;
+
+        public bool IsValidISBN(string isbn)
+        {
+            if (ContainsMixedSeperators(isbn)) return false;
+
+            string normalizedISBN = NormalizeISBN(isbn);
+
+            if (!ISBNHasValidLength(normalizedISBN)) return false;
+
+            var weightedSum = 0;
+            int checkSum = -1;
+
+            for (int i = 0; i < ISBN_13_LENGTH; i++)
+            {
+                if (!int.TryParse(normalizedISBN[i].ToString(), out int parsedDigit))
+                {
+                    return false;
+                }
+
+                if (IsCheckSumDigit(i))
+                {
+                    checkSum = parsedDigit;
+                }
+                else if (IsEven(i))
+                {
+                    weightedSum += parsedDigit * 3;
+                }
+                else
+                {
+                    weightedSum += parsedDigit * 1;
+                }
+            }
+
+            var calculatedChecksum = GetCalculatedChecksum(weightedSum);
+
+            return calculatedChecksum == checkSum;
+        }
+
+        private static bool ContainsMixedSeperators(string isbn)
+        {
+            return isbn.Contains(" ") && isbn.Contains("-");
+        }
+
+        private static bool IsEven(int i)
+        {
+            return (i + 1) % 2 == 0;
+        }
+
+        private static bool IsCheckSumDigit(int i)
+        {
+            return i == ISBN_13_LENGTH - 1;
+        }
+
+        private static int GetCalculatedChecksum(int weightedSum)
+        {
+            return (10 - (weightedSum % 10)) % 10;
+        }
+
+        private static bool ISBNHasValidLength(string normalizedISBN)
+        {
+            return normalizedISBN.Length == ISBN_13_LENGTH;
+        }
+
+        private static string NormalizeISBN(string isbn)
+        {
+            return isbn.Replace(" ", string.Empty).Replace("-", string.Empty);
+        }
+    }
+}
